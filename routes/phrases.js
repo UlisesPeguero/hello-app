d31492173b46d3c446bd8fda867b152d98c0c66a
UlisesPeguero <peguero.ulises@gmail.com>
2021-01-14 15:48:53 -0600

    Tested at http://localhost:3030/phrases

--- routes/phrases.js
+++ routes/phrases.js
@@ -4,9 +4,11 @@ const Phrase = require('../models/phrase');
 
 router.get('/', async (request, response) => {
     try {
-        const phrases = Phrase.find();
+        const phrases = await Phrase.find();
+        console.log(phrases);
         response.json(phrases);
     } catch(error) {
+        console.log(error);
         response.status(500).json({message: error.message});
     }
 });
